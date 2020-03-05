eea616c38312983f15ecdc73fe11ea8050a7cbc8
Thibault Desfontaines <desfontainesthibault@gmail.com>
2020-03-05 15:48:06 +0100

    Use TableauWriteHyper in the exporter

--- python-lib/export_schema_conversion.py
+++ python-lib/export_schema_conversion.py
@@ -1,3 +1,7 @@
+"""
+    Contains only the SchemaConversionDSSHyper class for conversion of the schemas
+"""
+
 from export_type_conversion import TypeConversionDSSHyper
 from tableauhyperapi import TableDefinition
 
@@ -9,82 +13,100 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format='Tableau Plugin | %(levelname)s - %(message)s')
 
 
+def is_geo(dss_schema):
+    """
+        Check if the dss schema contains a geopoint column in input.
+
+    :param dss_schema: A dss schema
+        expects the following input format: [{"name": "type"}] and returns the Tableau schema
+    :return: the corresponding Tableau Schema
+    """
+    for typed_column in dss_schema['columns']:
+        name, dss_type = typed_column['name'], typed_column['type']
+        if dss_type == 'geopoint':
+            return True
+    return False
+
+
+def dss_schema_geo_to_text(dss_schema):
+    """
+        Transform the input schema into a non geotable (regular table) in order to create a temporary table.
+
+    :param dss_schema: The input schema in dss
+    :return: The same dss schema as input but with all its geopoint columns as text columnss
+    """
+    mirror_schema = copy.deepcopy(dss_schema)
+    for typed_column in mirror_schema['columns']:
+        name, dss_type = typed_column['name'], typed_column['type']
+        if dss_type == 'geopoint':
+            typed_column['type'] = 'string'
+    return mirror_schema
+
+
 class SchemaConversionDSSHyper:
     """
+
         Convert the schema of a DSS table into a valid Tableau Schema.
-        A DSS schema is of the following format:
-                dss_schema = {'columns': [ {'name': 'order_date', 'type': 'string'}, ...], 'userModified': True}
+        TODO: Add a DSS schema and a Hyper schema to show the difference
+
+        Test location:
+            ./test/test_schema_conversion.py
+
     """
 
-    def __init__(self, debug=False):
+    def __init__(self):
         self.type_converter = TypeConversionDSSHyper()
         self.dss_types_array = []
         self.table = None
-        self.debug = debug
-
-    def is_geo(self, dss_schema):
-        """
-        Check if the dss schema includes a geo column.
-        :param dss_schema:
-        :return:
-        """
-        for typed_column in dss_schema['columns']:
-            name, dss_type = typed_column['name'], typed_column['type']
-            if dss_type == 'geopoint':
-                return True
-        return False
-
-    def dss_schema_geo_to_text(self, dss_schema):
-        """
-        Due to the specific load when handling a geo column, it is useful to be able to
-        create an intermediary dss schema with all the geopoint columns converted to text.
-        :param dss_schema:
-        :return: a dss schema with geopoint columns as text columns
-        """
-        mirror_schema = copy.deepcopy(dss_schema)
-        for typed_column in mirror_schema['columns']:
-            name, dss_type = typed_column['name'], typed_column['type']
-            if dss_type == 'geopoint':
-                typed_column['type'] = 'string'
-        return mirror_schema
 
     def convert_schema_dss_to_hyper(self, dss_schema):
         """
-        Conversion from the DSS Schema to the Hyper Schema. Both are data oriented table with
-        specific predefinition of the columns and their types.
-        :param dss_schema:
-        :return: A schema into the Hyper Format
+            Convert the input dss schema to a hyper file TableDefinition object.
+            Handle the conversion between columns types or storage types between DSS and Tableau.
+
+        :param dss_schema: The dss schema
+        :return: The hyper table TableDefinition object
         """
-        hyper_schema = []
+        hyper_columns = []
         for typed_column in dss_schema['columns']:
             name, dss_type = typed_column['name'], typed_column['type']
             hyper_type = self.type_converter.translate(dss_type)
-            hyper_schema.append(TableDefinition.Column(name, hyper_type))
-        return hyper_schema
+            hyper_columns.append(TableDefinition.Column(name, hyper_type))
+        return hyper_columns
 
     def set_dss_type_array(self, dss_schema):
+        """
+            Store a view of the columns types for later types conversions on row values
+
+        :param dss_schema:
+        :return: Does not return a value, only a setter
+        """
         self.dss_types_array = []
         for typed_column in dss_schema['columns']:
             name, dss_type = typed_column['name'], typed_column['type']
-            self.dss_types_array.append(dss_type) # Store a view of the types for later row conversion
+            self.dss_types_array.append(dss_type)
 
     def enforce_hyper_type_on_row(self, row):
         """
-            Cast the values on a single row.
-        :param row:
+            Do the types conversion on the values of the row.
+
+        :param row: The dss row (Should be an iterable)
         :return:
         """
+        # Convert the values of the dataset row
         output_row = [self.type_converter.set_type(value, dss_type) for value, dss_type in zip(row, self.dss_types_array)]
-        if self.debug:
-            logger.info("Enforce types on row:\n {} \n With dss types:\n  {} \n Output: \n {} \n".format(row, self.dss_types_array, output_row))
         return output_row
 
+
 if __name__ == "__main__":
+    # Show the use of the SchemaConversionDSSHyper class
     schema_converter = SchemaConversionDSSHyper()
     dss_schema = {'columns': [
-                    {'name': 'order_date', 'type': 'string'}, {'name': 'pages_visited', 'type': 'int'},
-                    {'name': 'order_id', 'type': 'string'}, {'name': 'customer_id', 'type': 'string'},
-                    {'name': 'tshirt_category', 'type': 'string'}, {'name': 'tshirt_price', 'type': 'float'},
-                    {'name': 'tshirt_quantity', 'type': 'int'}],
-               'userModified': True}
-    hyper_schema = schema_converter.convert_schema_dss_to_hyper(dss_schema)
+                    {'name': 'order_date', 'type': 'string'},
+                    {'name': 'pages_visited', 'type': 'bigint'},
+                    {'name': 'tshirt_quantity', 'type': 'unknown_format'}],
+        'userModified': True}
+    hyper_columns = schema_converter.convert_schema_dss_to_hyper(dss_schema)
+    for column in hyper_columns:
+        print(str(column.type))
+    pdb.set_trace()
