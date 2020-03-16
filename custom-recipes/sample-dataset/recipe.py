89a7f01f09943bbfc89e6ac74ceb91af6be35972
admin <admin@localhost>
2020-03-16 11:04:17 +0100

    Edited file 'custom-recipes/sample-dataset/recipe.py'

--- custom-recipes/sample-dataset/recipe.py
+++ custom-recipes/sample-dataset/recipe.py
@@ -1,15 +1,17 @@
 import dataiku
 from dataiku.customrecipe import *
+import pandas as pd
+import numpy as np
 
 output_names = get_output_names_for_role('main_output')
 output_dataset = dataiku.Dataset(output_names[0])
 
-my_variable = get_recipe_config()['n_rows']
-my_variable = get_recipe_config().get('n_rows', None)
+n_rows = get_recipe_config()['n_rows']
+N = 10**int(n_rows)
 
-import dataiku
-import pandas as pd, numpy as np
+int_serie = np.random.randint(0, 10000, size=N)
+float_serie = np.random.random(-1000, 1000, size=N)
 
-output = {"A": np.random.randint(0, 100, size=100)}
+output = {"int_serie": int_serie, "float_serie": float_serie}
 
 output_dataset.write_with_schema(pd.DataFrame(output))
