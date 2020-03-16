a1a35e146621767453153f887aa074c8dfdf760c
admin <admin@localhost>
2020-03-16 10:30:33 +0100

    Edited file 'custom-recipes/sample-dataset/recipe.py'

--- custom-recipes/sample-dataset/recipe.py
+++ custom-recipes/sample-dataset/recipe.py
@@ -7,14 +7,12 @@ input_A_datasets = [dataiku.Dataset(name) for name in input_A_names]
 output_names = get_output_names_for_role('main_output')
 output_dataset = dataiku.Dataset(output_names[0])
 
-my_variable = get_recipe_config()['parameter_name']
-my_variable = get_recipe_config().get('parameter_name', None)
+my_variable = get_recipe_config()['n_rows']
+my_variable = get_recipe_config().get('n_rows', None)
 
 import dataiku
 import pandas as pd, numpy as np
 
 output = {"A": np.random.randint(0, 100, size=100)}
 
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-output_dataset =  dataiku.Dataset("my_dataset")
 output_dataset.write_with_schema(pd.DataFrame(output))
