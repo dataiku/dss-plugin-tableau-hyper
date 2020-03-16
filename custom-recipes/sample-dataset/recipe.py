6d223d727dff420f998e99a6aa5671d759f53bb9
admin <admin@localhost>
2020-03-16 10:30:44 +0100

    Edited file 'custom-recipes/sample-dataset/recipe.py'

--- custom-recipes/sample-dataset/recipe.py
+++ custom-recipes/sample-dataset/recipe.py
@@ -1,9 +1,6 @@
 import dataiku
 from dataiku.customrecipe import *
 
-input_A_names = get_input_names_for_role('input_A_role')
-input_A_datasets = [dataiku.Dataset(name) for name in input_A_names]
-
 output_names = get_output_names_for_role('main_output')
 output_dataset = dataiku.Dataset(output_names[0])
 
