d41f18af444d07db8b15f86ddb76a1d84b49f721
admin <henri.chabert@dataiku.com>
2020-05-14 16:56:06 +0200

    Reverted to old method
    Fixed the pagination problem. Thought I could use the filtrer service from Tableau but it does not work it clients have
    ",", "&" or any special character usually used in URL QueryString.
    The solution is then to get all the projects and iterate till finding the right one or sending an
    error if not found after going through all the pages.

--- python-exporters/tableau-hyper_upload/exporter.py
+++ python-exporters/tableau-hyper_upload/exporter.py
@@ -7,36 +7,36 @@ from tableau_utils import TableauExport
 
 import tableauserverclient as TSC
 
-
 class CustomExporter(Exporter):
 
     def __init__(self, config, plugin_config):
         self.config = config
         self.plugin_config = plugin_config
         self.username = config.get('username', None)
-        self.password = config.get('password', '')
+        self.password = config.get('password','')
         self.server_url = config.get('server_url', None)
         self.project_name_utf8 = config.get('project', 'Default').encode('utf-8')
         self.output_table_utf8 = config.get('output_table', 'DSS_extract').encode('utf-8')
         self.site_id = config.get('site_id', '')
         self.ssl_cert_path = config.get('ssl_cert_path', None)
-
+        
+        
         if self.ssl_cert_path:
             if not os.path.isfile(self.ssl_cert_path):
                 raise ValueError('SSL certificate file %s does not exist' % self.ssl_cert_path)
             else:
-                # default variables handled by python requests to validate cert (used by underlying tableauserverclient)
+                #default variables handled by python requests to validate cert (used by underlying tableauserverclient)
                 os.environ['REQUESTS_CA_BUNDLE'] = self.ssl_cert_path
                 os.environ['CURL_CA_BUNDLE'] = self.ssl_cert_path
-
+        
         self.project = None
         self.datasource = None
-
+        
         if not (self.username and self.password and self.server_url):
             print('Connection params: {}'.format(
-                {'username:': self.username,
-                 'password:': '#' * len(self.password),
-                 'server_url:': self.server_url})
+                {'username:' : self.username,
+                'password:' : '#' * len(self.password),
+                'server_url:' : self.server_url})
             )
             raise ValueError("username, password and server_url shall not be empty")
 
@@ -50,43 +50,33 @@ class CustomExporter(Exporter):
         print("\tProduct version: {0}".format(s_info.product_version))
         print("\tREST API version: {0}".format(s_info.rest_api_version))
         print("\tBuild number: {0}".format(s_info.build_number))
-        print("\tAuth information: {0}".format(s_info.build_number))
-
+        
         server.auth.sign_in(tableau_auth)
-
-        project_req_option = TSC.RequestOptions()
-        project_req_option.filter.add(
-            TSC.Filter(
-                TSC.RequestOptions.Field.Name,
-                TSC.RequestOptions.Operator.Equals,
-                self.project_name_utf8
-            )
-        )
-
-        project_item, pagination_item = server.projects.get(req_options=project_req_option)
-        print("\tItems found: {0}".format(project_item))
-        if project_item:
-            self.project = project_item[0]
-            print('Found project matching {} with id {}'.format(self.project.name.encode('utf8'), self.project.id))
-        else:
-            # when project is not matched we want to print all accessible projects names for debugging
-            raise ValueError('Project {} does not exist on server'.format(self.project_name_utf8))
-
+        
+        all_project_items, pagination_item = server.projects.get()
+        page_nb = 1
+        while not self.project:
+            all_project_items, pag_it = server.projects.get(req_options=TSC.RequestOptions(pagenumber=page_nb))
+            filtered_projects = filter(lambda x: x.name.encode('utf-8') == self.project_name_utf8, all_project_items)
+            self.project = filtered_projects[0] if filtered_projects else None
+            if pag_it.page_number == (pag_it.total_available // pag_it.page_size) + 1 and not self.project:
+                raise ValueError('Project {} does not exist on server'.format(self.project_name_utf8))
+            page_nb += 1
+        
         all_datasources, pagination_item = server.datasources.get()
-
-        datasource_match = [d for d in all_datasources if d.name.encode('utf-8') == self.output_table_utf8]
+            
+        datasource_match = [d for d in all_datasources if d.name.encode('utf-8') == self.output_table_utf8 ]
         if len(datasource_match) > 0:
             self.datasource = datasource_match[0]
-            print('WARN: Found existing table {} with id {}, will be overwritten'.format(
-                self.datasource.name.encode('utf-8'), self.datasource.id))
+            print('WARN: Found existing table {} with id {}, will be overwritten'.format(self.datasource.name.encode('utf-8'), self.datasource.id))
 
         server.auth.sign_out()
-
+        
         # Fairly ugly. We create and delete a temporary file while retaining its name
         with tempfile.NamedTemporaryFile(prefix="output", suffix=".hyper", dir=os.getcwd()) as f:
             self.output_file = f.name
         print("Tmp file: {}".format(self.output_file))
-        self.e = TableauExport(self.output_file, schema['columns'])
+        self.e = TableauExport(self.output_file , schema['columns'])
 
     def write_row(self, row):
         """
@@ -94,7 +84,8 @@ class CustomExporter(Exporter):
         :param row: a tuple with N strings matching the schema passed to open.
         """
         self.e.insert_array_row(row)
-
+        
+        
     def close(self):
         """
         Perform any necessary cleanup
@@ -102,9 +93,9 @@ class CustomExporter(Exporter):
         self.e.close()
         tableau_auth = TSC.TableauAuth(self.username, self.password, site_id=self.site_id)
         server = TSC.Server(self.server_url, use_server_version=True)
-
+        
         server.auth.sign_in(tableau_auth)
-
+        
         try:
             if self.datasource:
                 server.datasources.publish(self.datasource, self.output_file, 'Overwrite', connection_credentials=None)
@@ -116,3 +107,4 @@ class CustomExporter(Exporter):
         finally:
             server.auth.sign_out()
             os.remove(self.output_file)
+
