dc6c0aa98929bf2c864975ed173772afa4c14df5
Mayeul Rousselet <mayeul.rousselet@dataiku.com>
2025-02-04 18:25:02 +0100

    refacto left over

--- tests/python/unit/test_schema_conversion.py
+++ tests/python/unit/test_schema_conversion.py
@@ -42,7 +42,7 @@ class TestSchemaConversion(TestCase):
     def test_hyper_columns_to_dss_columns(self):
         schema_converter = SchemaConversion()
         path_to_hyper = "data/superstore_sample.hyper"
-        hyper = get_hyper_process(Telemetry.DO_NOT_SEND_USAGE_DATA_TO_TABLEAU)
+        hyper = get_hyper_process()
         connection = Connection(hyper.endpoint, path_to_hyper)
         hyper_table = connection.catalog.get_table_definition(TableName('public', 'Customer'))
         connection.close()
